5a616aa4833cc88ec89d13c8e4e14f7635b3006a
lbarnettcq <lbarnett@cloudqnect.com>
2021-09-16 09:12:49 -0400

    Card 180 - Correct column sequence in Controller

--- force-app/main/default/aura/ToDoListLineItem/ToDoListLineItemController.js
+++ force-app/main/default/aura/ToDoListLineItem/ToDoListLineItemController.js
@@ -6,6 +6,19 @@
  * @version 1.0.0
  */
 ({
+    init : function(cmp, event, helper) {
+
+    } ,
+    onAnimalChange : function(cmp, event) {
+        var data = cmp.get('v.AnimalId');
+       cmp.set('v.AnimalUrlPath', '/' + data);
+       console.log(data);
+
+    } ,
+    onPlanChange : function (cmp, event) {
+        var data = cmp.get('v.PlanId');
+        cmp.set('v.TreatmentPlanURLPath', '/' + data);
+    } ,
     handleClick : function(cmp, event, helper) {
             var button = event.getSource().get('v.name');
             helper.buttonAction(cmp, event, button);
