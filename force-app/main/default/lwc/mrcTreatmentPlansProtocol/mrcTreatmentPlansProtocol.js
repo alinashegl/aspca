321e231ed7860b5952ef642098665c37af2314b5
Steven <steven.workman@aspca.org>
2022-03-31 07:48:01 -0600

    treatment plan report updates

--- force-app/main/default/lwc/mrcTreatmentPlansProtocol/mrcTreatmentPlansProtocol.js
+++ force-app/main/default/lwc/mrcTreatmentPlansProtocol/mrcTreatmentPlansProtocol.js
@@ -1,8 +1,11 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { updateRecord } from 'lightning/uiRecordApi';
+import getPlanProtocolInfo from '@salesforce/apex/MRCTreatmentPlansReportLWCController.getPlanProtocolInfo';
 import PROTOCOL_NAME_FIELD from '@salesforce/schema/Plan_Protocol__c.Protocol_Name__c';
 import PROTOCOL_NOTES_FIELD from '@salesforce/schema/Plan_Protocol__c.Protocol_Notes__c';
 import PROTOCOL_REMOVED_FIELD from '@salesforce/schema/Plan_Protocol__c.IsRemoved__c';
 import PROTOCOL_NOTES_MODIFIED_FIELD from '@salesforce/schema/Plan_Protocol__c.Notes_Last_Modified_Date__c';
+
 
 export default class MrcTreatmentPlansProtocol extends LightningElement {
     @api recordId;
@@ -11,4 +14,33 @@ export default class MrcTreatmentPlansProtocol extends LightningElement {
     protocolNotesField = PROTOCOL_NOTES_FIELD.fieldApiName;
     protocolRemovedField = PROTOCOL_REMOVED_FIELD.fieldApiName;
     protocolNotesModifiedDateField = PROTOCOL_NOTES_MODIFIED_FIELD.fieldApiName;
+
+    showSpinner = false;
+
+    @wire(getPlanProtocolInfo, { recordId: '$recordId' })
+    planProtocolInfo;
+
+    handleInputBlur(event){
+        window.console.log('blur');
+        this.showSpinner = true;
+
+        const fields = {};
+        fields[protocolNotesField] = event.target.value;
+        fields['Id'] = this.recordId;
+
+        const recordInput = { fields };
+
+        updateRecord(recordInput)
+        .then(() => {
+            window.console.log('success');
+        })
+        .catch(error => {
+            window.console.log('error: ', error.body.message);
+            this.errorMessage = 'Error updating Plan Protocol Notes:';
+            this.error = error;
+        })
+        .finally(() => {
+            this.showSpinner = false;
+        });
+    }
 }
--- force-app/main/default/lwc/mrcTreatmentPlansReportMain/mrcTreatmentPlansReportMain.js
+++ force-app/main/default/lwc/mrcTreatmentPlansReportMain/mrcTreatmentPlansReportMain.js
@@ -1,10 +1,12 @@
 import { LightningElement } from 'lwc';
 import getMRCDogs from '@salesforce/apex/MRCTreatmentPlansReportLWCController.getMRCDogs';
+import SystemModstamp from '@salesforce/schema/Account.SystemModstamp';
 
 export default class MrcTreatmentPlansReportMain extends LightningElement {
     animalList;
 
     connectedCallback(){
+        window.console.log('main - connected');
         if(this.animalList == undefined){
             getMRCDogs()
             .then((result) => {
