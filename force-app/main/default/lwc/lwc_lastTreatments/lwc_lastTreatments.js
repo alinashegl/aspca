35bacf8b89944137e289cac0deb213aa3a33398b
Steven Workman <steven.workman@aspca.org>
2022-08-17 08:46:41 -0600

    debugging

--- force-app/main/default/lwc/lwc_lastTreatments/lwc_lastTreatments.js
+++ force-app/main/default/lwc/lwc_lastTreatments/lwc_lastTreatments.js
@@ -1,8 +1,9 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import TREATMENT_PLAN_ID_FIELD from '@salesforce/schema/Session_Protocol__c.TreatmentSessionId__r.Treatment_Plan__c';
 import PROTOCOL_ID_FIELD from '@salesforce/schema/Session_Protocol__c.ProtocolId__c';
 import getLastTreatments from '@salesforce/apex/LastTreatmentsController.getLastTreatments';
+import SystemModstamp from '@salesforce/schema/ANH__AutoNumberConfigurationSetting__c.SystemModstamp';
 
 const fields = [TREATMENT_PLAN_ID_FIELD, PROTOCOL_ID_FIELD];
 
@@ -27,8 +28,41 @@ export default class Lwc_lastTreatments extends LightningElement {
         return getFieldValue(this.sessionProtocol.data, PROTOCOL_ID_FIELD);
     }
 
+    @track lastTreatments = [];
     @wire(getLastTreatments, { treatmentPlanId: '$treatmentPlanId', protocolId: '$protocolId', recordCount: '$recordCount'})
-    lastTreatments;
+    response(result){
+        if(result.data){
+            window.console.log('result: ', JSON.stringify(result));
+            console.table(result.data[0]);
+            this.lastTreatments = result.data;
+        }
+    }
+
+    // connectedCallback(){
+    //     window.console.log('**in connectedCallback');
+    //     window.console.log('**treatmentPlanId: ', this.treatmentPlanId);
+    //     window.console.log('**protocolId: ', this.protocolId);
+    //     window.console.log('**recordCount: ', this.recordCount);
+    //     this.recordCount = 5;
+    //     if(this.treatmentPlanId != null && this.protocolId != null && this.recordCount != null){
+    //         getLastTreatments({
+    //             treatmentPlanId: this.treatmentPlanId,
+    //             protocolId: this.protocolId,
+    //             recordCount: this.recordCount
+    //         })
+    //         .then((result) =>{
+    //             window.console.log('result: ', JSON.stringify(result));
+    //             this.lastTreatments = result;
+    //         })
+    //         .catch(error => {
+    //             // this.error = error;
+    //         })
+    //         .finally(() =>{
+    //             // this.showSpinner = false;
+    //         });
+    //     }
+    // }
+        
 
     handleToggle() {
         window.console.log('lastTreatments: ', JSON.stringify(this.lastTreatments));
