b82f9dd382046ca81a747b977d6dc2adf994d90f
Steven Workman <steven.workman@aspca.org>
2022-12-28 15:14:01 -0700

    Behavior Eval to LWC - tabs

--- force-app/main/default/lwc/behaviorEvaluationMain/behaviorEvaluationMain.js
+++ force-app/main/default/lwc/behaviorEvaluationMain/behaviorEvaluationMain.js
@@ -4,9 +4,8 @@ import getEvalTests from '@salesforce/apex/BehaviorEvaluationLWCController.getEv
 
 export default class BehaviorEvaluationMain extends LightningElement {
     @api recordId;
-    // evalTests;
     @track tabs = [];
-    activeTab;
+    currentTab;
     
     error;
     errorMessage;
@@ -14,114 +13,65 @@ export default class BehaviorEvaluationMain extends LightningElement {
     @wire(getEvalTests, {evalId : '$recordId'}) 
     response(result) {
         if(result.data){
-            // this.evalTests = result.data;
             this.tabs = JSON.parse(JSON.stringify(result.data.tabList));
             window.console.log('tabs: ', JSON.stringify(this.tabs));
-            this.activeTab = this.tabs[0].label;
+            this.selectedNavItem = this.tabs[0].label;
+            if(this.activeTab == undefined || this.activeTab == null){
+                this.activeTab = this.tabs[0];
+            }
         }
         else if(result.error){
             this.errorMessage = 'Error retrieving the evaluation and tests:';
             this.error = result.error;
         }
     }
-
-    renderedCallback(){
-        this.tabs.forEach(tab => {
-            let tabId = tab.id;
-            tabId = tabId.replace(/['"]+/g, '');
-            //cannot add color when value parameter is added to tab????
-            // this.addColorToIcon(tabId, tab.status);
-        });
-    }
-
-    handleComplete(event){
-        let tabId = event.target.dataset.id;
-        tabId = tabId.replace(/['"]+/g, '');
-
-        var foundIndex = this.tabs.findIndex(x => x.id == tabId);
-        this.tabs[foundIndex].iconName = "utility:success";
-
-        //cannot add color when value parameter is added to tab????
-        // this.addColorToIcon(tabId, "complete");
-    }
-
-    handleSkip(event){
-        let tabId = event.target.dataset.id;
-        tabId = tabId.replace(/['"]+/g, '');
-
-        var foundIndex = this.tabs.findIndex(x => x.id == tabId);
-        this.tabs[foundIndex].iconName = "utility:error";
-        //cannot add color when value parameter is added to tab????
-        // this.addColorToIcon(tabId, "skip");
-    }
-
-    addColorToIcon(tabId, status){
-        let tabIcon  = this.template.querySelector('[data-id="' +tabId+ '"]');
-        let tabIconId = tabIcon.getAttribute("aria-labelledby");
-        const firstStyleCss = document.createElement('style');
-        if(status == "complete"){
-            firstStyleCss.innerText = ` .tabsetCss #${tabIconId} .slds-icon-utility-success svg {
-                                    fill:green !important;
-                                    }
-                                `;
-        }
-        else if(status == "skip"){
-            firstStyleCss.innerText = ` .tabsetCss #${tabIconId} .slds-icon-utility-error svg {
-                fill:red !important;
-                }
-            `;
-        }
-        else{
-            firstStyleCss.innerText = ` .tabsetCss #${tabIconId} .slds-icon-utility-warning svg {
-                fill:orange !important;
-                }
-            `;
-        }
-
-        if (this.template.querySelector('lightning-tabset') != null) {
-            this.template.querySelector('lightning-tabset').appendChild(firstStyleCss);
-        }
-    }
-
-    handleButtonClick(event){
-        let info = event.target.dataset.id;
-        info = info.replace(/['"]+/g, '');
-        this.isComplete = true;
-        this.tabs[0].icon = "utility:success";
-
-        let tabIcon  = this.template.querySelector('[data-id="' +info+ '"]');
-        let tabIconId = tabIcon.getAttribute("aria-labelledby");
-        const firstStyleCss = document.createElement('style');
-        firstStyleCss.innerText = ` .tabsetCss #${tabIconId} .slds-icon-utility-success svg {
-                                fill:green !important;
-                                }
-                            `;
-        if (this.template.querySelector('lightning-tabset') != null) {
-            this.template.querySelector('lightning-tabset').appendChild(firstStyleCss);
-        }
-    }
-
-    activeTab = this.tabs
-
-    handleNext(event){
-        let tabId = event.target.dataset.id;
-        var foundIndex = this.tabs.findIndex(x => x.id == tabId);
-        if(foundIndex < this.tabs.length - 1){
-            this.activeTab = this.tabs[foundIndex + 1].label;
-        } else {
-            this.activeTab = this.tabs[0].label;
-        }
-    }
-
-    handlePrevious(event){
-        let tabId = event.target.dataset.id;
-        var foundIndex = this.tabs.findIndex(x => x.id == tabId);
-        if(foundIndex > 0){
-            this.activeTab = this.tabs[foundIndex - 1].label;
-        } else {
-            this.activeTab = this.tabs[this.tabs.length -1].label;
-        }
-    }
     
+    handleComplete(){
+        this.activeTab.status = "Success";
+        this.activeTab.iconName = "utility:success";
+    }
 
+    handleSkip(){
+        this.activeTab.status = "Error";
+        this.activeTab.iconName = "utility:error";
+
+    }
+
+    handleNext(){
+        // let tabId = event.target.dataset.id;
+        var foundIndex = this.tabs.findIndex(x => x.label == this.selectedNavItem);
+        if(foundIndex < this.tabs.length - 1){
+            this.activeTab = this.tabs[foundIndex + 1];
+            this.selectedNavItem = this.activeTab.label;
+        } else {
+            this.activeTab = this.tabs[0];
+            this.selectedNavItem = this.activeTab.label;
+        }
+    }
+
+    handlePrevious(){
+        var foundIndex = this.tabs.findIndex(x => x.label == this.selectedNavItem);
+        if(foundIndex > 0){
+            this.activeTab = this.tabs[foundIndex - 1];
+            this.selectedNavItem = this.activeTab.label;
+        } else {
+            this.activeTab = this.tabs[this.tabs.length -1];
+            this.selectedNavItem = this.activeTab.label;
+        }
+    }
+
+
+    /****vertical navigation */
+    activeTab;
+    selectedNavItem = "In Kennel";
+    handleNavigationSelect(event){
+        const selectedName = event.detail.name;
+        this.selectedNavItem = selectedName;
+        this.setActiveTab();
+    }
+
+    setActiveTab(){
+        var foundIndex = this.tabs.findIndex(x => x.label == this.selectedNavItem);
+        this.activeTab = this.tabs[foundIndex];
+    }
 }
